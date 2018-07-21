a1fa694addebdab846781dbf6c97a60e26133b2e
Michael Droogleever <droogmic@gmail.com>
2018-07-22 01:26:23 +0200

    fix mdbook versioning and index.hbs

--- ci/install.sh
+++ ci/install.sh
@@ -5,7 +5,7 @@ main() {
         sh -s -- \
            --force \
            --git rust-lang-nursery/mdBook \
-           --tag v0.1.5 \
+           --tag v0.1.8 \
            --target x86_64-unknown-linux-musl
 
     rustup target add thumbv6m-none-eabi
