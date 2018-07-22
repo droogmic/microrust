b81ea7156d337010968192c0773f57bb947ef0d3
Michael Droogleever <droogmic@gmail.com>
2018-07-22 02:04:39 +0200

    Fix install script with full build

--- ci/install.sh
+++ ci/install.sh
@@ -1,12 +1,20 @@
 set -euxo pipefail
 
 main() {
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git rust-lang-nursery/mdBook \
-           --tag v0.1.8 \
-           --target x86_64-unknown-linux-musl
+
+    if [ "$(mdbook -V)" != "mdbook v0.1.8" ]; then
+        curl -LSfs https://japaric.github.io/trust/install.sh | \
+            sh -s -- \
+            --force \
+            --git rust-lang-nursery/mdBook \
+            --tag v0.1.8 \
+            --target x86_64-unknown-linux-musl \
+        || true
+    fi
+
+    if [ "$(mdbook -V)" != "mdbook v0.1.8" ]; then
+        cargo install --target x86_64-unknown-linux-gnu --version 0.1.8 mdbook
+    fi
 
     rustup target add thumbv6m-none-eabi
 
