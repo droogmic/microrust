ea52aa2ea6791fc3d04c1244dc5eedfb81ac694a
Michael Droogleever Fortuyn <droogmic@gmail.com>
2019-11-30 20:38:09 +0100

    autofix

--- src/serial/src/main.rs
+++ src/serial/src/main.rs
@@ -21,7 +21,7 @@ fn main() -> ! {
     writeln!(stdout, "Start").unwrap();
     if let Some(p) = microbit::Peripherals::take() {
         // Split GPIO
-        let mut gpio = p.GPIO.split();
+        let gpio = p.GPIO.split();
         // Configure RX and TX pins accordingly
         let tx = gpio.pin24.into_push_pull_output().downgrade();
         let rx = gpio.pin25.into_floating_input().downgrade();
